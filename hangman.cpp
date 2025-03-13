df489238cf3583726513c875f1c67bf2ab951995
Apham130 <andypham4747@gmail.com>
2025-03-13 20:49:20 +0000

    almost done, there is a bug in getHInt function

--- hangman.cpp
+++ hangman.cpp
@@ -11,9 +11,40 @@
 #include <string>    // std::string, std::getline
 #include <vector>    // std::vector
 
-std::vector<std::string> getHint(std::string secret, char guessLetter, std::vector<std::string> hint)
-{
+std::vector<std::string> wordList = {
+    "education",
+    "solution",
+    "mountain",
+    "platinum",
+    "alphabet",
+    "necklace",
+    "elevator",
+    "diamonds",
+    "fragrant",
+    "velocity",
+    "graduate",
+    "nebraska",
+    "tropical",
+    "evidence",
+    "involved",
+    "shopping",
+    "baseball",
+    "kangaroo",
+    "horrible",
+    "animals",
+    "supercalifragilisticexpialidocious"
+};
 
+std::vector<std::string> getHint(std::vector<char> wordList, char letter, std::vector<std::string> hint)
+{
+    for (int index = 0; index < length(wordList), index++;)
+    {
+        if (wordList[index] == letter)
+        {
+            hint[index] = letter;
+        }
+    }
+    
     return hint;
 }
 
@@ -31,7 +62,7 @@ void hangman(int lives)
         << " |               " << std::endl
         << " |               " << std::endl
         << " |               " << std::endl
-        << " |               " << std::endl; 
+        << " |________________" << std::endl; 
     } else if (lives == 5) 
     {
         std::cout << "_________________" << std::endl
@@ -44,7 +75,7 @@ void hangman(int lives)
         << " |               " << std::endl
         << " |               " << std::endl
         << " |               " << std::endl
-        << " |               " << std::endl;  
+        << " |________________" << std::endl;  
     } else if (lives == 4) 
     {
         std::cout << "_________________" << std::endl
@@ -57,7 +88,7 @@ void hangman(int lives)
         << " |               " << std::endl
         << " |               " << std::endl
         << " |               " << std::endl
-        << " |               " << std::endl;  
+        << " |________________" << std::endl;  
     } else if (lives == 3) 
     {
         std::cout << "_________________" << std::endl
@@ -70,7 +101,7 @@ void hangman(int lives)
         << " |               " << std::endl
         << " |               " << std::endl
         << " |               " << std::endl
-        << " |               " << std::endl;  
+        << " |________________" << std::endl;  
     } else if (lives == 2) 
     {
         std::cout << "_________________" << std::endl
@@ -83,7 +114,7 @@ void hangman(int lives)
         << " |               " << std::endl
         << " |               " << std::endl
         << " |               " << std::endl
-        << " |               " << std::endl;  
+        << " |________________" << std::endl;  
     } else if (lives == 1) 
     {
         std::cout << "_________________" << std::endl
@@ -96,7 +127,7 @@ void hangman(int lives)
         << " |               " << std::endl
         << " |               " << std::endl
         << " |               " << std::endl
-        << " |               " << std::endl;  
+        << " |________________" << std::endl;  
     } else if (lives == 0) 
     {
         std::cout << "_________________" << std::endl
@@ -109,55 +140,90 @@ void hangman(int lives)
         << " |               " << std::endl
         << " |               " << std::endl
         << " |               " << std::endl
-        << " |               " << std::endl;  
+        << " |________________" << std::endl;  
     }
+}
+
+bool endGame(bool won, int lives)
+{
+    if (won == true)
+    {
+        return true;
+    } else if (lives == 0)
+    {
+        return true;
+    }
+    return false;
 }
 
 
 
 int main()
 {
-    std::vector<std::string> wordList = {
-        "education",
-        "solution",
-        "mountain",
-        "platinum",
-        "alphabet",
-        "necklace",
-        "elevator",
-        "diamonds",
-        "fragrant",
-        "velocity",
-        "graduate",
-        "nebraska",
-        "tropical",
-        "evidence",
-        "involved",
-        "shopping",
-        "baseball",
-        "kangaroo",
-        "horrible",
-        "animals"
-    };
-    
+
     srand(time(0));
     std::string theWord = wordList[rand() % length(wordList)];
     std::vector<std::string> hint = std::vector<std::string>(theWord.length(), "_");
+    std::vector<std::string> tempHint = hint;
     int lives = 6;
+    bool gameWin = false;
+    std::vector<char> wordButList;
+    for (char c : theWord)
+    {
+        wordButList.push_back(c);
+    }
 
-    std::cout << "Welcome to Hangman!\n";
+    std::cout << "Welcome to Hangman!\n(To guess the entire word, input 0)\n";
     hangman(lives);
     display(hint); 
+
+    bool canGuess = true;
     
-    while (true)
+    while (!endGame(gameWin, lives))
     {
         char letterGuess;
-        std::cout << "\n> ";
+        char thisThing = '0';
+        std::cout << theWord <<std::endl;
+        tempHint = hint;
+        std::cout << " \nGuess a letter:\n> ";
         std::cin >> letterGuess;
-        hint = getHint(theWord, letterGuess, hint);
+        if (letterGuess == thisThing && canGuess == true)
+        {
+            std::string guess;
+            std::cout << "What do you think the word is?\n> ";
+            std::cin >> guess;
+            if (guess == theWord)
+            {
+                gameWin = true;
+            }
+            canGuess = false;
+        
+        } else
+        {
+            hint = getHint(wordButList, letterGuess, hint);
+            canGuess = true;
+            if (tempHint == hint)
+            {
+                lives = lives - 1;
+            }
+        }
         hangman(lives);
         display(hint); 
-
     }
+    
+    if (lives == 0)
+    {
+        std::cout << "Sorry, you lost!\nThe word was '" << theWord <<  "'.\n";
+    } else if (lives == 6)
+    {
+        std::cout << "Cheater\n";
+    } else if (lives == 1)
+    {
+        std::cout << "Congrats, you won!\n You had 1 life left!\n";
+    } else
+    {
+        std::cout << "Congrats, you won!\n You had " << lives << " lives left!\n";
+    }
+
     return 0;
 }
